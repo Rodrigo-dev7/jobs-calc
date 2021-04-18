f6864f032604577bc737c559ea748ba3211f95d9
rodrigo <rodrigo@pop-os.localdomain>
2021-04-18 19:24:20 -0300

    add ejs

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const routes = express.Router()
 
+const views = __dirname + '/views/'
+
+const profile = {
+  name: 'Rodrigo',
+  avatar: 'https://avatars.githubusercontent.com/u/60434681?v=4',
+  "monthly-budget": 3000,
+  "hours-per-day": 5,
+  "days-per-week": 9,
+  "vacation-per-year": 4,
+}
+
 // request, response
-routes.get('/', (request, response) => {
-  return response.sendFile(__dirname + '/views/index.html')
-})
+routes.get('/', (req, res) => res.render(views + 'index'))
+routes.get('/job', (req, res) => res.render(views + 'job'))
+routes.get('/job/edit', (req, res) => res.render(views + 'job-edit'))
+routes.get('/profile', (req, res) => res.render(views + 'profile', { profile }))
 
-
-routes.get('/index.html', (req, res) => {
-  return res.redirect('/')
-})
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -2,11 +2,12 @@ const express = require('express')
 const server = express()
 const routes = require('./routes')
 
+server.set('view engine', 'ejs')
+
 // habilitar arquivos stactic
 server.use(express.static('public'))
 
 // routes
 server.use(routes)
-
-server.listen(3001, () => console.log('Running...'))
+server.listen(3000, () => console.log('Running...'))
 
